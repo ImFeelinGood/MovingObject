90cb30dc29d7da9a92adbd037d9550df46317d70
Daffa Yahya <daffayahya2003@gmail.com>
2024-09-11 10:13:29 +0700

    Initial commit

---
+++ MovingObjectClient/ClientForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovingObjectClient
+{
+    public partial class ClientForm : Form
+    {
+        private Rectangle rect = new Rectangle(20, 20, 30, 30);
+        private Pen red = new Pen(Color.Red);
+        private SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        private Socket clientSocket;
+        private byte[] buffer;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            ConnectToServer();
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.BeginConnect("127.0.0.1", 3333, ConnectCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket.EndConnect(AR);
+                buffer = new byte[clientSocket.ReceiveBufferSize];
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                int received = clientSocket.EndReceive(AR);
+                if (received > 0)
+                {
+                    // Asumsi data yang diterima dalam format "X,Y"
+                    string data = Encoding.ASCII.GetString(buffer, 0, received);
+                    string[] positionData = data.Split(',');
+
+                    if (positionData.Length == 2)
+                    {
+                        int newX, newY;
+
+                        // Parse X dan Y dari data yang diterima
+                        if (int.TryParse(positionData[0], out newX) && int.TryParse(positionData[1], out newY))
+                        {
+                            // Update posisi kubus di client
+                            rect.X = newX;
+                            rect.Y = newY;
+                            Invalidate(); // Memicu repaint agar kubus diupdate
+                        }
+                    }
+
+                    // Lanjut menerima data dari server
+                    clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClientForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+    }
+}
---
+++ MovingObjectServer/ServerForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class ServerForm : Form
+    {
+        // Pen, Rectangle, dan variabel untuk pergerakan
+        Pen red = new Pen(Color.Red);
+        Rectangle rect = new Rectangle(20, 20, 30, 30);
+        SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        int slide = 10;
+
+        // Socket server untuk komunikasi
+        private Socket serverSocket;
+        private List<Socket> clientSockets = new List<Socket>();
+        private byte[] buffer = new byte[1024];
+
+        public ServerForm()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+
+            // Memulai server socket
+            StartServer();
+        }
+
+        // Memulai server untuk mendengarkan koneksi klien
+        private void StartServer()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 3333));
+            serverSocket.Listen(5);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        // Menerima koneksi dari klien
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+            clientSockets.Add(clientSocket);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        // Mengirimkan data posisi ke klien
+        private void SendDataToClients()
+        {
+            string positionData = $"{rect.X},{rect.Y}";  // Kirim posisi X dan Y
+            byte[] data = Encoding.ASCII.GetBytes(positionData);
+
+            foreach (var client in clientSockets)
+            {
+                try
+                {
+                    client.Send(data);
+                }
+                catch (SocketException)
+                {
+                    // Jika terjadi kesalahan, tutup socket dan keluarkan dari list
+                    client.Close();
+                    clientSockets.Remove(client);
+                    break;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Menggerakkan kubus
+            rect.X += slide;
+            back();
+
+            // Kirim data posisi ke semua klien
+            SendDataToClients();
+
+            // Menggambar ulang di server
+            Invalidate();
+        }
+
+        private void back()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else if (rect.X <= rect.Width / 2)
+                slide = 10;
+        }
+
+        // Menggambar objek di form
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+    }
+}
